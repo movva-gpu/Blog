29d852a746c64f95e94f2dc083762e6205a34796
Danyella <danyellastrikann@duck.com>
2024-07-02 14:57:41 +0200

    fix(frontend): :bug: adds keywords from the config and removes useless code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import '$lib/fonts.css';
 	import '../app.css';
+
 	import Header from './Header.svelte';
-	import * as en from '$lib/lang/en';
 	import Footer from './Footer.svelte';
 
-	export const lang = en;
+	import { keywords } from '$lib/config';
 </script>
+
+<svelte:head>
+	<meta name="keywords" content={keywords} />
+</svelte:head>
 
 <Header />
 <main>
